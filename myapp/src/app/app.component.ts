25ed7ac7c2872968f9673c216bb0d45966bd716b
Readri205 <read205@outlook.com>
2021-04-12 14:23:23 +0000

    add meta tags for viewport

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+
+  constructor (
+    private metaService: Meta
+  ) { }
+
+  public setCanScale(canScale: boolean): void {
+    // this.metaService.removeTag('name=viewport');
+    if (canScale) {
+      this.metaService.updateTag({
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, user-scalable=yes'
+      }, 'name=viewport');
+    } else {
+      this.metaService.updateTag({
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+      }, 'name=viewport');
+    }
+  }
 }
